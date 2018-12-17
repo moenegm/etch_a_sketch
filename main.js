e71d44df8f863d20da45bae884483c29e52f70e0
Mohamed Negm <mr.negm90@gmail.com>
2018-12-17 18:09:32 -0500

    Final Game

--- main.js
+++ main.js
@@ -1,56 +1,58 @@
-const container = document.querySelector(".container"),
-  body = document.querySelector("body"),
-  clear = document.createElement("button"),
-  one = document.createElement("div"),
-  two = document.createElement("div"),
-  three = document.createElement("div"),
-  four = document.createElement("div");
+// Grid System
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+const grid = document.createElement("div");
+
+//Button Declartion.
+const clear = document.createElement("button");
 clear.textContent = "Clear selection";
+
+clear.className = "button";
+
+//Appending
 container.appendChild(clear);
-container.appendChild(one);
-container.appendChild(two);
-container.appendChild(three);
-container.appendChild(four);
+container.appendChild(grid);
 
-one.style.cssText = "border: 1px solid black;";
-two.style.cssText = "border: 1px solid black;";
-three.style.cssText = "border: 1px solid black;";
-four.style.cssText = "border: 1px solid black;";
+//Generting Boxes in the Grid
+function generateBoxes(length) {
+  for (let i = 1; i <= length * length; i++) {
+    let box = document.createElement("div");
+    box.classList.add("box");
+    grid.appendChild(box);
+    box.style.cssText = "border: 1px solid #cccccc; height: 10px;";
+  }
+}
+generateBoxes(70);
 
+//Styling
 container.style.cssText =
-  "display: grid; grid-template-columns: repeat(2, 1fr); grid-template-rows: repeat(4, 1fr); margin: auto; ";
+  "height: 100%; margin: auto; background-color: #ffffff; border: 1px solid black;";
 clear.style.cssText =
-  "grid-column-start: span 2; text-align: center; border: 1px solid black; padding: 20px; font-weight: bold;";
-body.style.cssText = "margin: 10%;";
+  "text-align: center; border-bottom: 1px solid black; padding: 20px; font-weight: bold; width: 100%; background-color: #ff6600";
+body.style.cssText = "margin: 10%; height: 100%; background-color: #cccccc";
+grid.style.cssText = "display: grid; grid-template-columns: repeat(100, 1fr);";
 
-one.classList.add("hover");
-two.classList.add("hover");
-three.classList.add("hover");
-four.classList.add("hover");
-
-one.addEventListener("mouseover", oneHoverFunction);
-two.addEventListener("mouseover", twoHoverFunction);
-three.addEventListener("mouseover", threeHoverFunction);
-four.addEventListener("mouseover", fourHoverFunction);
-
-function oneHoverFunction() {
-  one.style.cssText = "border: 1px solid black; background: yellow;";
+// Button Effects
+clear.addEventListener("click", clearFunction);
+clear.addEventListener("mouseover", buttonOnFunction);
+clear.addEventListener("mouseout", buttonOutFunction);
+function buttonOnFunction(event) {
+  clear.style.opacity = "0.5";
 }
-function twoHoverFunction() {
-  two.style.cssText = "border: 1px solid black; background: yellow;";
+function buttonOutFunction(event) {
+  clear.style.opacity = "1";
 }
-function threeHoverFunction() {
-  three.style.cssText = "border: 1px solid black; background: yellow;";
-}
-function fourHoverFunction() {
-  four.style.cssText = "border: 1px solid black; background: yellow;";
+function clearFunction(event) {
+  grid.innerHTML = "";
+  generateBoxes(70);
 }
 
-/*
-one.addEventListener("mouseout", hoverOutFunction);
+// Coloring, actual game.
 
-function hoverOutFunction() {
-  one.style.cssText = "width: 100px; height: 100px; background: red;";
-}
-*/
-//container.style.display = "grid";
+document.body.addEventListener("mouseover", e => {
+  if (e.target.classList.contains("box")) {
+    e.target.style.backgroundColor = "black";
+  }
+});
+
+// Cheating.
