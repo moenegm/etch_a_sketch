809bbdc378306c071181c0dbe48468d2d7f8c64b
Mohamed Negm <mr.negm90@gmail.com>
2018-12-17 19:10:29 -0500

    finished with instructions

--- main.js
+++ main.js
@@ -3,13 +3,26 @@ const body = document.querySelector("body");
 const container = document.querySelector(".container");
 const grid = document.createElement("div");
 
+// Header Section
+const h1Directions = document.createElement("h1");
+const pDirections = document.createElement("p");
+h1Directions.className = "h1";
+pDirections.className = "p";
+
+//Directions:
+
+h1Directions.textContent = "Moe's Etch-A-Sketch";
+pDirections.textContent =
+  "Directions: hover mouse over the grid to draw a beatiful picture. If you want to start over click the large orange button that says 'clear section'.\r\n\r\nPSST: listen, I know it's a little boring like this so I added a cheat code. Click on thescreen to erase the box you are currently over. This way you get to have more fun!";
+
 //Button Declartion.
 const clear = document.createElement("button");
 clear.textContent = "Clear selection";
-
 clear.className = "button";
 
 //Appending
+container.appendChild(h1Directions);
+container.appendChild(pDirections);
 container.appendChild(clear);
 container.appendChild(grid);
 
@@ -28,9 +41,11 @@ generateBoxes(70);
 container.style.cssText =
   "height: 100%; margin: auto; background-color: #ffffff; border: 1px solid black;";
 clear.style.cssText =
-  "text-align: center; border-bottom: 1px solid black; padding: 20px; font-weight: bold; width: 100%; background-color: #ff6600";
+  "text-align: center; border-bottom: 1px solid black; border-top: 1px solid black; padding: 20px; font-weight: bold; width: 100%; background-color: #ff6600";
 body.style.cssText = "margin: 10%; height: 100%; background-color: #cccccc";
 grid.style.cssText = "display: grid; grid-template-columns: repeat(100, 1fr);";
+h1Directions.style.cssText = "text-align: center;";
+pDirections.setAttribute("style", "white-space: pre; text-align: center;");
 
 // Button Effects
 clear.addEventListener("click", clearFunction);
